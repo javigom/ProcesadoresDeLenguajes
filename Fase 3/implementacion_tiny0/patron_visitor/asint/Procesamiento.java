8ecd1f09df636ef2adc229cd6b4e40758604b8f8
Javier Gómez <elgomez182@gmail.com>
2022-04-24 14:05:39 +0200

    TinyAsint finished.

--- Fase 3/implementacion_tiny0/patron_visitor/asint/Procesamiento.java
+++ Fase 3/implementacion_tiny0/patron_visitor/asint/Procesamiento.java
@@ -1,18 +1,34 @@
 package asint;
 
 import asint.TinyASint.Suma;
+import asint.TinyASint.True;
 import asint.TinyASint.Resta;
 import asint.TinyASint.Mul;
+import asint.TinyASint.Not;
 import asint.TinyASint.Div;
+import asint.TinyASint.False;
 import asint.TinyASint.Id;
-import asint.TinyASint.Num;
-import asint.TinyASint.Dec;
+import asint.TinyASint.Igual;
+import asint.TinyASint.Instruccion;
+import asint.TinyASint.Insts_muchas;
+import asint.TinyASint.Insts_una;
+import asint.TinyASint.LitEnt;
+import asint.TinyASint.LitReal;
+import asint.TinyASint.Mayor;
+import asint.TinyASint.MayorIgual;
+import asint.TinyASint.Menor;
+import asint.TinyASint.MenorIgual;
+import asint.TinyASint.MenosUnario;
+import asint.TinyASint.Or;
+import asint.TinyASint.And;
+import asint.TinyASint.Declaracion;
 import asint.TinyASint.Decs_muchas;
 import asint.TinyASint.Decs_una;
-import asint.TinyASint.Prog_sin_decs;
-import asint.TinyASint.Prog_con_decs;
+import asint.TinyASint.Distinto;
+import asint.TinyASint.Programa;
 
 public interface Procesamiento {
+<<<<<<< Updated upstream
     void procesa(Suma exp);
     void procesa(Resta exp);
     void procesa(Mul exp);
@@ -26,4 +42,69 @@ public interface Procesamiento {
     void procesa(Decs_una decs);
     void procesa(Prog_sin_decs prog);    
     void procesa(Prog_con_decs prog);    
+=======
+
+	// Operadores
+
+	// Nivel 0
+	void procesa(Suma exp);
+
+	void procesa(Resta exp);
+
+	// Nivel 1
+	void procesa(And exp);
+
+	void procesa(Or exp);
+
+	// Nivel 2
+	void procesa(Menor exp);
+
+	void procesa(Mayor exp);
+
+	void procesa(MenorIgual exp);
+
+	void procesa(MayorIgual exp);
+
+	void procesa(Igual exp);
+
+	void procesa(Distinto exp);
+
+	// Nivel 3
+	void procesa(Mul exp);
+
+	void procesa(Div exp);
+
+	// Nivel 4
+	void procesa(MenosUnario exp);
+
+	void procesa(Not exp);
+
+	// Nivel 5
+	void procesa(True exp);
+
+	void procesa(False exp);
+
+	void procesa(LitEnt exp);
+
+	void procesa(LitReal exp);
+
+	void procesa(Id exp);
+
+	// Declaraciones
+	void procesa(Declaracion declaracion);
+
+	void procesa(Decs_muchas decs);
+
+	void procesa(Decs_una decs);
+
+	// Instrucciones
+	void procesa(Instruccion instruccion);
+
+	void procesa(Insts_muchas insts);
+
+	void procesa(Insts_una insts);
+
+	// Programa
+	void procesa(Programa programa);
+>>>>>>> Stashed changes
 }
--- Fase 3/implementacion_tiny0/patron_visitor/asint/TinyASint.java
+++ Fase 3/implementacion_tiny0/patron_visitor/asint/TinyASint.java
@@ -1,242 +1,647 @@
 package asint;
 
-
-
 public class TinyASint {
-    
 
-    public static abstract class Exp  {
-       public Exp() {
-       }   
-       public abstract int prioridad();
-       public abstract void procesa(Procesamiento procesamiento);
-    }
-    
-    public static class StringLocalizado {
-     private String s;
-     private int fila;
-     private int col;
-     public StringLocalizado(String s, int fila, int col) {
-         this.s = s;
-         this.fila = fila;
-         this.col = col;
-     }
-     public int fila() {return fila;}
-     public int col() {return col;}
-     public String toString() {
-       return s;
-     }
-     public boolean equals(Object o) {
-         return (o == this) || (
-                (o instanceof StringLocalizado) &&
-                (((StringLocalizado)o).s.equals(s)));                
-     }
-     public int hashCode() {
-         return s.hashCode();
-     }
-  }
+	public static abstract class Exp {
+		public Exp() {
+		}
 
-    
-    private static abstract class ExpBin extends Exp {
-        private Exp arg0;
-        private Exp arg1;
-        public Exp arg0() {return arg0;}
-        public Exp arg1() {return arg1;}
-        public ExpBin(Exp arg0, Exp arg1) {
-            super();
-            this.arg0 = arg0;
-            this.arg1 = arg1;
-        }
-    }
-        
-    private static abstract class ExpAditiva extends ExpBin {
-        public ExpAditiva(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public final int prioridad() {
-            return 0;
-        }
-    }
-    
-    public static class Suma extends ExpAditiva {
-        public Suma(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static class Resta extends ExpAditiva {
-        public Resta(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    
-    private static abstract class ExpMultiplicativa extends ExpBin {
-        public ExpMultiplicativa(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public final int prioridad() {
-            return 1;
-        }
-    }
-    
-    public static class Mul extends ExpMultiplicativa {
-        public Mul(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static class Div extends ExpMultiplicativa {
-        public Div(Exp arg0, Exp arg1) {
-            super(arg0,arg1);
-        }
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static class Num extends Exp {
-        private StringLocalizado num;
-        public Num(StringLocalizado num) {
-            super();
-            this.num = num;
-        }
-        public StringLocalizado num() {return num;}
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-        public final int prioridad() {
-            return 2;
-        }
-    }
-    public static class Id extends Exp {
-        private StringLocalizado id;
-        public Id(StringLocalizado id) {
-            super();
-            this.id = id;
-        }
-        public StringLocalizado id() {return id;}
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-        public final int prioridad() {
-            return 2;
-        }
-    }
-    public static class Dec  {
-        private StringLocalizado id;
-        private StringLocalizado val;
-        public Dec(StringLocalizado id, StringLocalizado val) {
-            this.id = id;
-            this.val = val;
-        }
-        public StringLocalizado id() {return id;}
-        public StringLocalizado val() {return val;}
-        public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static abstract class Decs {
-       public Decs() {
-       }
-       public abstract void procesa(Procesamiento p);
-    }
-    public static class Decs_una extends Decs {
-       private Dec dec; 
-       public Decs_una(Dec dec) {
-          super();
-          this.dec = dec;
-       }   
-       public Dec dec() {return dec;}
-       public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static class Decs_muchas extends Decs {
-       private Dec dec;
-       private Decs decs;
-       public Decs_muchas(Decs decs, Dec dec) {
-          super();
-          this.dec = dec;
-          this.decs = decs;
-       }
-       public Dec dec() {return dec;}
-       public Decs decs() {return decs;}
-       public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static abstract class Prog  {
-       public Prog() {
-       }   
-       public abstract void procesa(Procesamiento p); 
-    }
-    public static class Prog_sin_decs extends Prog {
-      private Exp exp;
-       public Prog_sin_decs(Exp exp) {
-          super();
-          this.exp = exp;
-       }   
-       public Exp exp() {return exp;}
-       public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
-    public static class Prog_con_decs extends Prog {
-      private Exp exp;
-      private Decs decs;
-       public Prog_con_decs(Exp exp, Decs decs) {
-          super();
-          this.exp = exp;
-          this.decs = decs;
-       }   
-       public Exp exp() {return exp;}
-       public Decs decs() {return decs;}
-       public void procesa(Procesamiento p) {
-           p.procesa(this); 
-        }     
-    }
+		public abstract int prioridad();
 
-     // Constructoras    
-    public Prog prog_con_decs(Exp exp, Decs decs) {
-        return new Prog_con_decs(exp,decs);
-    }
-    public Prog prog_sin_decs(Exp exp) {
-        return new Prog_sin_decs(exp);
-    }
-    public Exp suma(Exp arg0, Exp arg1) {
-        return new Suma(arg0,arg1);
-    }
-    public Exp resta(Exp arg0, Exp arg1) {
-        return new Resta(arg0,arg1);
-    }
-    public Exp mul(Exp arg0, Exp arg1) {
-        return new Mul(arg0,arg1);
-    }
-    public Exp div(Exp arg0, Exp arg1) {
-        return new Div(arg0,arg1);
-    }
-    public Exp num(StringLocalizado num) {
-        return new Num(num);
-    }
-    public Exp id(StringLocalizado num) {
-        return new Id(num);
-    }
-    public Dec dec(StringLocalizado id, StringLocalizado val) {
-        return new Dec(id,val);
-    }
-    public Decs decs_una(Dec dec) {
-        return new Decs_una(dec);
-    }
-    public Decs decs_muchas(Decs decs, Dec dec) {
-        return new Decs_muchas(decs,dec);
-    }
-    public StringLocalizado str(String s, int fila, int col) {
-        return new StringLocalizado(s,fila,col);
-    }
+		public abstract void procesa(Procesamiento procesamiento);
+	}
+
+	public static class StringLocalizado {
+		private String s;
+		private int fila;
+		private int col;
+
+		public StringLocalizado(String s, int fila, int col) {
+			this.s = s;
+			this.fila = fila;
+			this.col = col;
+		}
+
+		public int fila() {
+			return fila;
+		}
+
+		public int col() {
+			return col;
+		}
+
+		public String toString() {
+			return s;
+		}
+
+		public boolean equals(Object o) {
+			return (o == this) || ((o instanceof StringLocalizado) && (((StringLocalizado) o).s.equals(s)));
+		}
+
+		public int hashCode() {
+			return s.hashCode();
+		}
+	}
+	
+	// Operadores
+
+	private static abstract class ExpBin extends Exp {
+		private Exp arg0;
+		private Exp arg1;
+
+		public Exp arg0() {
+			return arg0;
+		}
+
+		public Exp arg1() {
+			return arg1;
+		}
+
+		public ExpBin(Exp arg0, Exp arg1) {
+			super();
+			this.arg0 = arg0;
+			this.arg1 = arg1;
+		}
+	}
+	
+
+	// Nivel 0
+
+	private static abstract class ExpAditiva extends ExpBin {
+		public ExpAditiva(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public final int prioridad() {
+			return 0;
+		}
+	}
+
+	public static class Suma extends ExpAditiva {
+		public Suma(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Resta extends ExpAditiva {
+		public Resta(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	
+	// Nivel 1
+
+	private static abstract class ExpLogica extends ExpBin {
+		public ExpLogica(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public final int prioridad() {
+			return 1;
+		}
+	}
+
+	public static class And extends ExpLogica {
+		public And(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Or extends ExpLogica {
+		public Or(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	
+	// Nivel 2
+
+	private static abstract class ExpRelacional extends ExpBin {
+		public ExpRelacional(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public final int prioridad() {
+			return 2;
+		}
+	}
+
+	public static class Menor extends ExpRelacional {
+		public Menor(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Mayor extends ExpRelacional {
+		public Mayor(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class MenorIgual extends ExpRelacional {
+		public MenorIgual(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class MayorIgual extends ExpRelacional {
+		public MayorIgual(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Igual extends ExpRelacional {
+		public Igual(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Distinto extends ExpRelacional {
+		public Distinto(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	
+	// Nivel 3
+
+	private static abstract class ExpMultiplicativa extends ExpBin {
+		public ExpMultiplicativa(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public final int prioridad() {
+			return 3;
+		}
+	}
+
+	public static class Mul extends ExpMultiplicativa {
+		public Mul(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Div extends ExpMultiplicativa {
+		public Div(Exp arg0, Exp arg1) {
+			super(arg0, arg1);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	
+	// Nivel 4
+	
+	private static abstract class ExpUnaria extends Exp {
+		private Exp arg0;
+
+		public Exp arg0() {
+			return arg0;
+		}
+
+		public ExpUnaria(Exp arg0) {
+			super();
+			this.arg0 = arg0;
+		}
+		
+		public final int prioridad() {
+			return 4;
+		}
+	}
+
+	public static class MenosUnario extends ExpUnaria {
+		public MenosUnario(Exp arg0) {
+			super(arg0);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	public static class Not extends ExpUnaria {
+		public Not(Exp arg0) {
+			super(arg0);
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	
+	// Nivel 5
+
+	public static class True extends Exp {
+		public True() {
+			
+		}
+		
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+		
+		public final int prioridad() {
+			return 5;
+		}
+	}
+	
+	public static class False extends Exp {
+		public False() {
+			
+		}
+		
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+		
+		public final int prioridad() {
+			return 5;
+		}
+	}
+
+	public static class LitEnt extends Exp {
+		private StringLocalizado ent;
+
+		public LitEnt(StringLocalizado ent) {
+			super();
+			this.ent = ent;
+		}
+
+		public StringLocalizado num() {
+			return ent;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+
+		public final int prioridad() {
+			return 5;
+		}
+	}
+	
+	public static class LitReal extends Exp {
+		private StringLocalizado real;
+
+		public LitReal(StringLocalizado real) {
+			super();
+			this.real = real;
+		}
+
+		public StringLocalizado num() {
+			return real;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+
+		public final int prioridad() {
+			return 5;
+		}
+	}
+
+	public static class Id extends Exp {
+		private StringLocalizado id;
+
+		public Id(StringLocalizado id) {
+			super();
+			this.id = id;
+		}
+
+		public StringLocalizado id() {
+			return id;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+
+		public final int prioridad() {
+			return 5;
+		}
+	}
+	
+	
+	// Declaraciones
+
+	public static class Declaracion {
+		private StringLocalizado id;
+		private StringLocalizado val;
+
+		public Declaracion(StringLocalizado id, StringLocalizado val) {
+			this.id = id;
+			this.val = val;
+		}
+
+		public StringLocalizado id() {
+			return id;
+		}
+
+		public StringLocalizado val() {
+			return val;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static abstract class Declaraciones {
+		public Declaraciones() {
+		}
+
+		public abstract void procesa(Procesamiento p);
+	}
+
+	public static class Decs_una extends Declaraciones {
+		private Declaracion declaracion;
+
+		public Decs_una(Declaracion declaracion) {
+			super();
+			this.declaracion = declaracion;
+		}
+
+		public Declaracion declaracion() {
+			return declaracion;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Decs_muchas extends Declaraciones {
+		private Declaracion declaracion;
+		private Declaraciones declaraciones;
+
+		public Decs_muchas(Declaraciones declaraciones, Declaracion declaracion) {
+			super();
+			this.declaraciones = declaraciones;
+			this.declaracion = declaracion;
+		}
+
+		public Declaracion declaracion() {
+			return declaracion;
+		}
+
+		public Declaraciones declaraciones() {
+			return declaraciones;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	// Instrucciones
+	
+	public static class Instruccion {
+		private StringLocalizado id;
+		private Exp exp;
+
+		public Instruccion(StringLocalizado id, Exp exp) {
+			this.id = id;
+			this.exp = exp;
+		}
+
+		public StringLocalizado id() {
+			return id;
+		}
+
+		public Exp exp() {
+			return exp;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	public static abstract class Instrucciones {
+		public Instrucciones() {
+		}
+
+		public abstract void procesa(Procesamiento p);
+	}
+
+	public static class Insts_una extends Instrucciones {
+		private Instruccion instruccion;
+
+		public Insts_una(Instruccion instruccion) {
+			super();
+			this.instruccion = instruccion;
+		}
+
+		public Instruccion instruccion() {
+			return instruccion;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+
+	public static class Insts_muchas extends Instrucciones {
+		private Instruccion instruccion;
+		private Instrucciones instrucciones;
+
+		public Insts_muchas(Instrucciones instrucciones, Instruccion instruccion) {
+			super();
+			this.instrucciones = instrucciones;
+			this.instruccion = instruccion;
+		}
+
+		public Instrucciones instrucciones() {
+			return instrucciones;
+		}
+
+		public Instruccion instruccion() {
+			return instruccion;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+	// Programa
+
+	public static class Programa {
+		private Declaraciones declaraciones;
+		private Instrucciones instrucciones;
+
+		public Programa(Declaraciones declaraciones, Instrucciones instrucciones) {
+			super();
+			this.declaraciones = declaraciones;
+			this.instrucciones = instrucciones;
+		}
+
+		public Declaraciones declaraciones() {
+			return declaraciones;
+		}
+		
+		public Instrucciones instrucciones() {
+			return instrucciones;
+		}
+
+		public void procesa(Procesamiento p) {
+			p.procesa(this);
+		}
+	}
+	
+
+	// Constructoras
+	
+	public Programa Programa(Declaraciones declaraciones, Instrucciones instrucciones) {
+		return new Programa(declaraciones, instrucciones);
+	}
+
+	public Exp suma(Exp arg0, Exp arg1) {
+		return new Suma(arg0, arg1);
+	}
+
+	public Exp resta(Exp arg0, Exp arg1) {
+		return new Resta(arg0, arg1);
+	}
+	
+	public Exp and_cons(Exp arg0, Exp arg1) {
+		return new And(arg0, arg1);
+	}
+	
+	public Exp or_cons(Exp arg0, Exp arg1) {
+		return new Or(arg0, arg1);
+	}
+	
+	public Exp menor(Exp arg0, Exp arg1) {
+		return new Menor(arg0, arg1);
+	}
+	
+	public Exp mayor(Exp arg0, Exp arg1) {
+		return new Mayor(arg0, arg1);
+	}
+	
+	public Exp menorIgual(Exp arg0, Exp arg1) {
+		return new MenorIgual(arg0, arg1);
+	}
+	
+	public Exp mayorIgual(Exp arg0, Exp arg1) {
+		return new MayorIgual(arg0, arg1);
+	}
+	
+	public Exp igual(Exp arg0, Exp arg1) {
+		return new Igual(arg0, arg1);
+	}
+	
+	public Exp distinto(Exp arg0, Exp arg1) {
+		return new Distinto(arg0, arg1);
+	}
+	
+	public Exp mul(Exp arg0, Exp arg1) {
+		return new Mul(arg0, arg1);
+	}
+
+	public Exp div(Exp arg0, Exp arg1) {
+		return new Div(arg0, arg1);
+	}
+	
+	public Exp menosUnario(Exp arg0) {
+		return new MenosUnario(arg0);
+	}
+
+	public Exp true_cons() {
+		return new True();
+	}
+	
+	public Exp false_cons() {
+		return new False();
+	}
+	
+	public Exp litEnt(StringLocalizado arg0) {
+		return new LitEnt(arg0);
+	}
+	
+	public Exp litReal(StringLocalizado arg0) {
+		return new LitReal(arg0);
+	}
+	
+	public Exp id(StringLocalizado arg0) {
+		return new Id(arg0);
+	}
+	
+	public Declaracion declaracion(StringLocalizado id, StringLocalizado val) {
+		return new Declaracion(id, val);
+	}
+
+	public Declaraciones decs_una(Declaracion declaracion) {
+		return new Decs_una(declaracion);
+	}
+
+	public Declaraciones decs_muchas(Declaraciones declaraciones, Declaracion declaracion) {
+		return new Decs_muchas(declaraciones, declaracion);
+	}
+	
+	public Instruccion instruccion(StringLocalizado id, Exp exp) {
+		return new Instruccion(id, exp);
+	}
+
+	public Instrucciones insts_una(Instruccion instruccion) {
+		return new Insts_una(instruccion);
+	}
+
+	public Instrucciones insts_muchas(Instrucciones instrucciones, Instruccion instruccion) {
+		return new Insts_muchas(instrucciones, instruccion);
+	}
+	
+	public StringLocalizado str(String s, int fila, int col) {
+		return new StringLocalizado(s, fila, col);
+	}
 }
